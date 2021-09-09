983b2ca3a3fd1c8f8b3992971c6dbad20a1e71b4
Menashe <mborukhov770@gmail.com>
2021-09-09 23:18:36 +0300

    vowelCount complete

--- app.js
+++ app.js
@@ -7,9 +7,27 @@ new Set([1, 1, 2, 2, 3, 4]); // {1, 2, 3, 4}
 //#3
 let m = new Map();
 m.set([1, 2, 3], true);
-m.set([1, 2, 3], false); // {Array(3) => true, Array(3) => false} OR {[1,2,3]: true, [1,2,3]:false}
+m.set([1, 2, 3], false); // {Array(3) => true, Array(3) => false} OR {[1, 2, 3]: true, [1, 2, 3]: false}
 
 //
 const hasDuplicate = (arr) => new Set(arr).size !== arr.length;
 
 //
+function isVowel(char) {
+  return 'aeiou'.includes(char);
+}
+
+function vowelCount(string) {
+  const newMap = new Map();
+  let strLower = string.toLowerCase();
+  for (x of strLower) {
+    if (isVowel(x)) {
+      if (newMap.has(x)) {
+        newMap.set(x, newMap.get(x) + 1);
+      } else {
+        newMap.set(x, 1);
+      }
+    }
+  }
+  return newMap;
+}
