c5db282a7dfc05c63e1a0766e740983366131838
Menashe <mborukhov770@gmail.com>
2021-09-09 21:19:03 +0300

    hasDuplicate complete

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-new Set([1,1,2,2,3,4]) // {1, 2, 3, 4}
+// #1
+new Set([1, 1, 2, 2, 3, 4]); // {1, 2, 3, 4}
 
-[...new Set("referee")].join("") // 'ref'
+// #2
+[...new Set('referee')].join(''); // 'ref'
 
+//#3
 let m = new Map();
-m.set([1,2,3], true);
-m.set([1,2,3], false); // {Array(3) => true, Array(3) => false}
+m.set([1, 2, 3], true);
+m.set([1, 2, 3], false); // {Array(3) => true, Array(3) => false} OR {[1,2,3]: true, [1,2,3]:false}
+
+//
+const hasDuplicate = (arr) => new Set(arr).size !== arr.length;
+
+//
